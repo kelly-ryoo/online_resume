068f66421c0452f405cece50b49df8332892565f
kelly-ryoo <kyyr.0317@gmail.com>
2020-09-01 16:23:52 -0400

    first commit

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php include_once('header.php') ?>
+
+<div class="cover">
+    <div class="row logo" id="pt0">
+        <div class="col-sm-2"><a href="home.php"><img src="img/logo.png" width="30%"></a></div>
+    </div>
+
+    <div class="row name" id="pt1">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4"><h1 class="name">Kelly Ryoo</h1></div>
+        <div class="col-sm-6"></div>
+    </div>
+</div>
+
+<div class="nav" id="sidenav">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About Me</a></li>
+    <li><a href="#">Education</a></li>
+    <li><a href="#">Experience</a></li>
+    <li><a href="#">Projects</a></li>
+  </ul>
+</div>
+
+<div class="aboutMe">
+    <div class="row text-center">
+        <div><h1>About Me</h1><div>
+        <hr></hr>
+    </div>
+</div>
+
+<?php include_once('footer.php') ?>
